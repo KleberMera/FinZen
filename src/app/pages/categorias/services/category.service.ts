01e21d7dbfc80ef41354d12e5deef4a23cb0c613
KleberMera <klebermera2016@gmail.com>
2024-12-28 01:44:00 -0500

    icons form to json

--- src/app/pages/categorias/services/category.service.ts
+++ src/app/pages/categorias/services/category.service.ts
@@ -5,7 +5,7 @@ import { Category } from '@models/category';
 import { apiResponse } from '@models/apiResponse';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { environment } from '@environments/environment.development';
-
+import iconsData from '../../../../../public/icons.json';
 export interface primeIcons {
   icon: string;
 }
@@ -17,43 +17,8 @@ export interface primeIcons {
 //Servicio de categorias
 export class CategoryService {
   private apiUrl = environment.apiUrl;
-  public readonly primeIcons = signal<primeIcons[]>([
-    { icon: 'pi pi-users' },
-    { icon: 'pi pi-shopping-cart' },
-    { icon: 'pi pi-shop' },
-    { icon: 'pi pi-credit-card' },
-    { icon: 'pi pi-calendar' },
-    { icon: 'pi pi-check' },
-    { icon: 'pi pi-cloud' },
-    { icon: 'pi pi-exclamation-triangle' },
-    { icon: 'pi pi-external-link' },
-    { icon: 'pi pi-file' },
-    { icon: 'pi pi-flag' },
-    { icon: 'pi pi-heart' },
-    { icon: 'pi pi-info-circle' },
-    { icon: 'pi pi-lock' },
-    { icon: 'pi pi-map-marker' },
-    { icon: 'pi pi-minus' },
-    { icon: 'pi pi-paperclip' },
-    { icon: 'pi pi-pencil' },
-    { icon: 'pi pi-plus' },
-    { icon: 'pi pi-search' },
-    { icon: 'pi pi-star' },
-    { icon: 'pi pi-tag' },
-    { icon: 'pi pi-tags' },
-    { icon: 'pi pi-trash' },
-    { icon: 'pi pi-user' },
-    { icon: 'pi pi-user-edit' },
-    { icon: 'pi pi-user-minus' },
-    { icon: 'pi pi-user-plus' },
-    { icon: 'pi pi-volume-down' },
-    { icon: 'pi pi-volume-off' },
-    { icon: 'pi pi-volume-up' },
-    { icon: 'pi pi-wifi' },
-    { icon: 'pi pi-wrench' },
-  ]);
+  public readonly primeIcons = signal<primeIcons[]>(iconsData);
   private readonly htpp = inject(HttpClient);
-
   public getPrimeIcons() {
     return this.primeIcons;
   }
