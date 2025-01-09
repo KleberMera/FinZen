dc17946fa7457827ed11820e01a36b825f68e7c5
KleberMera <klebermera2016@gmail.com>
2025-01-09 17:26:57 -0500

    list deb componet filters

---
+++ src/app/pages/debt/components/filters-debt/filters-debt.component.ts
@@ -0,0 +1,67 @@
+
+import {
+  Component,
+  computed,
+  effect,
+  input,
+  output,
+  signal,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { Debt } from '@models/debt';
+
+@Component({
+  selector: 'filters-debt',
+  imports: [FormsModule],
+  templateUrl: './filters-debt.component.html',
+  styleUrl: './filters-debt.component.scss',
+})
+export class FiltersDebtComponent {
+  // Input signal para las transacciones
+  readonly debts = input.required<Debt[]>();
+
+  // Output event para los filtros
+  readonly filterChange = output<{
+    name: string;
+  }>();
+
+  protected readonly nameFilter = signal<string>('');
+
+  protected readonly uniqueNames = computed(() => {
+    return [...new Set(this.debts().map((t) => t.name))];
+  });
+
+  protected readonly types = ['Ingreso', 'Gasto'];
+
+  constructor() {
+    // Observar cambios en los filtros y emitirlos
+    effect(() => {
+      this.filterChange.emit({
+        name: this.nameFilter(),
+      });
+    });
+
+    document.addEventListener('click', (event: Event) => {
+      const target = event.target as HTMLElement;
+      if (!target.closest('[id$="-dropdown"]')) {
+        document.querySelectorAll('[id$="-dropdown-menu"]').forEach((menu) => {
+          menu.classList.add('hidden');
+        });
+      }
+    });
+  }
+
+  toggleDropdown(dropdownId: 'name'): void {
+    const menu = document.getElementById(`${dropdownId}-dropdown-menu`);
+    const allMenus = document.querySelectorAll('[id$="-dropdown-menu"]');
+
+    allMenus.forEach((element) => {
+      if (element.id !== `${dropdownId}-dropdown-menu`) {
+        element.classList.add('hidden');
+      }
+    });
+
+    menu?.classList.toggle('hidden');
+  }
+}
--- src/app/pages/debt/ui/list-debt/list-debt.component.ts
+++ src/app/pages/debt/ui/list-debt/list-debt.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { DebtService } from '../../services/debt.service';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { StorageService } from '@services/storage.service';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { FiltersDebtComponent } from "../../components/filters-debt/filters-debt.component";
 
+export interface Filter {
+  
+  name: string;
+ 
+}
 @Component({
   selector: 'app-list-debt',
-  imports: [],
+  imports: [DatePipe, CurrencyPipe, FiltersDebtComponent],
   templateUrl: './list-debt.component.html',
-  styleUrl: './list-debt.component.scss'
+  styleUrl: './list-debt.component.scss',
 })
 export class ListDebtComponent {
+  private readonly _debtService = inject(DebtService);
+   protected readonly filters = signal<Filter>({ name: '' });
+  private readonly seletedUser = signal<number>(
+    inject(StorageService).getUserId()
+  );
+  readonly totalMonths = signal<number>(0);
+  ngOnInit() {
+    //const amortizations = this.formData().get('amortizations')?.value || [];
+    
+   // this.totalMonths.set(amortizations.length);
+  }
+  readonly debts = rxResource({
+    request: () => ({ userId: this.seletedUser() }),
+    loader: ({ request }) => this._debtService.getDebtsByUserId(request.userId),
+  });
 
+
+  onFilterChange(newFilters: { name: string; }) {
+    this.filters.set(newFilters);
+  }
 }
