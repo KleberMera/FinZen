7df764f676ad1237ff14754049dcaff76a6a11d1
KleberMera <klebermera2016@gmail.com>
2025-01-17 17:46:58 -0500

    delete funtion sidebar

--- src/app/shared/layout/sidebar/sidebar.component.ts
+++ src/app/shared/layout/sidebar/sidebar.component.ts
@@ -2,22 +2,13 @@ import { Component, input, output } from '@angular/core';
 import { SidebarItemsComponent } from '../sidebar-items/sidebar-items.component';
 import { SidebarConfComponent } from '../sidebar-conf/sidebar-conf.component';
 
-
-
 @Component({
   selector: 'app-sidebar',
   imports: [SidebarItemsComponent],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
   isOpen = input(false);
   closeSidebar = output<void>();
-
-  onItemSelected() {
-    if (window.innerWidth < 768) {
-      this.closeSidebar.emit();
-    }
-  }
- 
 }
