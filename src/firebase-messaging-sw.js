f1a04336212a40fc8112ff8e31ba7b086b62abfb
KleberMera <klebermera2016@gmail.com>
2025-02-07 17:28:50 -0500

    feat: agregar soporte para notificaciones en segundo plano; incluir archivo de service worker de Firebase

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getMessaging } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-sw.js";
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging";
 
 const firebaseApp = initializeApp({
   apiKey: 'AIzaSyCzisdNiu6Q2L6vvs5mFvcSWelZVZWOuaU',
