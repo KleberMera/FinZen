44a98da97d12821b019f534db48a25ec79e42001
KleberMera <klebermera2016@gmail.com>
2025-02-08 01:40:57 -0500

    feat: mejorar manejo de suscripciones a notificaciones; enviar suscripción al backend y manejar respuestas en el servicio de notificaciones push

--- src/app/services/push-notification.service.ts
+++ src/app/services/push-notification.service.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Auth } from '@angular/fire/auth';
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
 import { environment } from '@environments/environment';
 import { SwPush } from '@angular/service-worker';
 
@@ -9,64 +7,34 @@ import { SwPush } from '@angular/service-worker';
   providedIn: 'root'
 })
 export class PushNotificationService {
-  readonly VAPID_PUBLIC_KEY = 'BAuc-ujSh7gHMDIyT4jaCtx-Z-db1gITpVLCfDCCz3f7haRs8rxUoDdS9vzpjApkr8Uey4bKv4XJ0Z0JAlXPLFE';
+  readonly VAPID_PUBLIC_KEY = 'BB9RG9no5eZuqpw0mqNNTRdo1gzSQJAhVKsI2X8SDuUHnHAKcO8co6UWPkZwykP7OINeSSV3IiN_hjVj_kwhaLM';
 
-  constructor(private swPush: SwPush) {
-  }
+  constructor(
+    private swPush: SwPush,
+    private http: HttpClient
+  ) {}
 
-  ngOnInit(): void {
-    this.onEventNotifications();
-  }
-
-  enableNotify() {
-    if (this.swPush.isEnabled) {
-      try {
-        if (Notification.permission != 'granted') {
-          this.requestSubscription();
-        } else {
-          const isConfirm = confirm("Ya tiene los permisos para notificar. ¿Deseas refrescar el token?");
-          if (isConfirm) this.requestSubscription();
+  async requestSubscription() {
+    try {
+      const sub = await this.swPush.requestSubscription({
+        serverPublicKey: this.VAPID_PUBLIC_KEY
+      });
+      
+      // Enviamos la suscripción al backend
+      this.http.post(
+        `${environment.apiUrl}/notifications/subscribe`, 
+        sub
+      ).subscribe(
+        {
+          next: () => console.log('Subscription sent to server', sub),
+          error: err => console.error('Could not send subscription to server', err)
         }
-      } catch (error: any) {
-        alert('No se estableció los permisos para notificar');
-        console.error(error.message);
-      }
-    } else {
-      console.error("El service worker no ha sido cargado");
-    }
-  }
-
-  unsubscribe() {
-    if (this.swPush.isEnabled && Notification.permission == 'granted')
-      this.swPush.unsubscribe();
-  }
-
-  getToken() {
-    if (this.swPush.isEnabled && Notification.permission == 'granted')
-      this.swPush.subscription.subscribe((subscription) => {
-        console.log(subscription);
-      });
-  }
-
-  private async requestSubscription() {
-    const payload = await this.swPush.requestSubscription({ serverPublicKey: environment.vapidKey });
-    console.log(payload.toJSON());
-    this.onEventNotifications();
-    //? Hacer petición para guardar token en el backend
-  }
-
-  private onEventNotifications() {
-    if (this.swPush.isEnabled && Notification.permission == 'granted') {
-
-      this.swPush.notificationClicks.subscribe((data) => {
-
-        console.log(data);
-      })
-
-      this.swPush.messages.subscribe((message: any) => {
-        console.log(message);
-        // console.log(message.notification.data.message);
-      });
+      );
+      
+      return true;
+    } catch (err) {
+      console.error('Could not subscribe to notifications', err);
+      return false;
     }
   }
 }
