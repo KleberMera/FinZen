bd6de7711aa74f00295f6bb19628107b4ece52dc
KleberMera <klebermera2016@gmail.com>
2025-02-13 17:40:01 -0500

    feat: implementar modo de edición en el sidebar de perfil de usuario; agregar controles de formulario para editar datos personales

--- src/app/shared/layout/navbar/components/user-profile-sidebar/user-profile-sidebar.component.ts
+++ src/app/shared/layout/navbar/components/user-profile-sidebar/user-profile-sidebar.component.ts
@@ -1,24 +1,60 @@
+import { CommonModule, NgIf } from '@angular/common';
 import { Component, EventEmitter, output, Output, signal } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile-sidebar',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, NgIf],
   templateUrl: './user-profile-sidebar.component.html',
   styleUrl: './user-profile-sidebar.component.scss'
 })
 export class UserProfileSidebarComponent {
   closeSidebar = output<void>();
+  editMode = signal(false);
   
-  // Datos random (puedes reemplazar con datos reales luego)
   userData = signal({
+    id: 1,
+    name: 'John',
+    last_name: 'Doe',
+    username: 'johndoe',
+    email: 'john@example.com',
+    phone: '+1234567890',
+    role: 'Administrador',
+    avatar: 'https://randomuser.me/api/portraits/men/91.jpg',
+    status: true,
+    createdAt: new Date(),
+    // ... otros campos del modelo
+  });
 
-      name: 'John Doe',
-      email: 'john@example.com',
-      role: 'Administrador',
-      avatar: 'https://randomuser.me/api/portraits/men/75.jpg',
-      lastLogin: 'Hace 2 horas'
-  
-  })
+  // Controles del formulario
+  nameControl = new FormControl(this.userData().name);
+  usernameControl = new FormControl(this.userData().username);
+  emailControl = new FormControl(this.userData().email);
+  phoneControl = new FormControl(this.userData().phone);
+
+  enableEdit() {
+    this.editMode.set(true);
+  }
+
+  cancelEdit() {
+    this.editMode.set(false);
+    // Restablecer valores
+    this.nameControl.setValue(this.userData().name);
+    this.usernameControl.setValue(this.userData().username);
+    this.emailControl.setValue(this.userData().email);
+    this.phoneControl.setValue(this.userData().phone);
+  }
+
+  saveChanges() {
+    this.userData.update(data => ({
+      ...data,
+      name: this.nameControl.value || '',
+      username: this.usernameControl.value || '',
+      email: this.emailControl.value || '',
+      phone: this.phoneControl.value || ''
+    }));
+    this.editMode.set(false);
+  }
 
   close() {
     this.closeSidebar.emit();
