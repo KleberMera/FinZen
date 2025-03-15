713328da82394b4414545d7ab7625fa328416cff
KleberMera <klebermera2016@gmail.com>
2025-03-15 01:53:03 -0500

    feat: agregar animación de deslizamiento al sidebar de perfil de usuario; mejorar la lógica de cierre y navegación

--- src/app/shared/layout/navbar/components/user-profile-sidebar/user-profile-sidebar.component.ts
+++ src/app/shared/layout/navbar/components/user-profile-sidebar/user-profile-sidebar.component.ts
@@ -1,18 +1,24 @@
-import { CommonModule, NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, inject, output, signal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ReactiveFormsModule } from '@angular/forms';
 import { StorageService } from '@services/storage.service';
 import { UserProfileService } from '../../services/user-profile.service';
 import { AuthService } from '../../../../../auth/services/auth.service';
-import { tap } from 'rxjs';
+
 import { apiResponse } from '@models/apiResponse';
 import { User } from '@models/user';
-import { UserProfileSidebarSkeletonComponent } from "../user-profile-sidebar-skeleton/user-profile-sidebar-skeleton.component";
+import { UserProfileSidebarSkeletonComponent } from '../user-profile-sidebar-skeleton/user-profile-sidebar-skeleton.component';
+import { Router } from '@angular/router';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-user-profile-sidebar',
-  imports: [CommonModule, ReactiveFormsModule, UserProfileSidebarSkeletonComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    UserProfileSidebarSkeletonComponent,
+  ],
   templateUrl: './user-profile-sidebar.component.html',
   styleUrl: './user-profile-sidebar.component.scss',
 })
@@ -24,42 +30,19 @@ export class UserProfileSidebarComponent {
   private readonly seletedUser = signal<number>(
     inject(StorageService).getUserId()
   );
-  protected formUser = this._authService.formUser();
+  private readonly _router = inject(Router);
 
   closeSidebar = output<void>();
-  editMode = signal(false);
 
   public user = rxResource<apiResponse<User>, { userId: number }>({
     request: () => ({ userId: this.seletedUser() }),
     loader: ({ request }) =>
-      this._userProfileService.getUserProfile(request.userId).pipe(
-        tap((res) => {
-          if (res.data) {
-            this.formUser().patchValue(res.data);
-          }
-        })
-      ),
+      this._userProfileService.getUserProfile(request.userId),
   });
 
-  enableEdit() {
-    this.editMode.set(true);
-  }
-
-  cancelEdit() {
-    this.editMode.set(false);
-
-    // Restablecer valores
-    //this.nameControl.setValue(this.userData().name);
-  }
-
-  logout(){
-
-  }
-
-  saveChanges() {
-    this.editMode.set(false);
-    console.log(this.formUser().value);
-    
+  logout() {
+    this._router.navigate(['auth']);
+    toast.success('Sesión cerrada');
   }
 
   close() {
