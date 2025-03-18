d1867c30d8ca06118114499d1825e69fc5d0e01a
KleberMera <klebermera2016@gmail.com>
2025-03-17 23:07:13 -0500

    feat: agregar servicio de gráficos y componente de gráficos avanzados; incluir estilos y pruebas

---
+++ src/app/core/models/grafic.ts
@@ -0,0 +1,5 @@
+export interface WeeklyDataItem {
+    day: string; // Día de la semana
+    gasto: number; // Gastos en el día
+    ingreso: number; // Ingresos en el día
+  }
---
+++ src/app/pages/dashboard/services/grafics.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { apiResponse } from '@models/apiResponse';
+import { WeeklyDataItem } from '@models/grafic';
+
+import { Observable, of, switchMap, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GraficsService {
+  protected readonly _http = inject(HttpClient);
+
+  getGraficWeeklYByUserId(
+    userId: number
+  ): Observable<apiResponse<WeeklyDataItem[]>> {
+    const url = `${environment.apiUrl}/grafic/weekly-summary/${userId}`;
+    return this._http.get<apiResponse<WeeklyDataItem[]>>(url);
+  }
+}
