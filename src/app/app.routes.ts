3f41818de144ea1dc67142209c2a0b446540eb8b
KleberMera <klebermera2016@gmail.com>
2025-03-25 12:16:23 -0500

    feat: reestructurar rutas para mejorar la navegación y utilizar constantes para las páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,23 +2,55 @@ import { Routes } from '@angular/router';
 import { authRoutes } from './auth/auth.routes';
 import { pagesRoutes } from './pages/pages.routes';
 
+export enum FEATURE_PAGES {
+  AUTH = 'auth',
+  HOME = 'home',
+}
 
 export const routes: Routes = [
-  {
-    path: 'auth',
-    children: authRoutes,
-  },
-  {
-    path: 'home',
-    children: pagesRoutes,
-  },
+  // {
+  //   path: 'auth',
+  //   children: authRoutes,
+  // },
+  // {
+  //   path: 'home',
+  //   children: pagesRoutes,
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: 'auth',
+  //   pathMatch: 'full',
+  // },
+  // {
+  //   path: '**',
+  //   redirectTo: 'auth',
+  // }
   {
     path: '',
-    redirectTo: 'auth',
-    pathMatch: 'full',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: FEATURE_PAGES.AUTH,
+        // loadChildren: () => import('./auth/auth.routes').then(m => m.authRoutes)
+      },
+      {
+        path: FEATURE_PAGES.AUTH,
+        loadChildren: () =>
+          import('./auth/auth.routes').then((m) => m.authRoutes),
+      },
+      {
+        path: FEATURE_PAGES.HOME,
+        loadChildren: () =>
+          import('./pages/pages.routes').then((m) => m.pagesRoutes),
+      },
+      {
+        path: '**',
+        redirectTo: FEATURE_PAGES.AUTH,
+      },{
+        path: '**',
+        redirectTo: FEATURE_PAGES.HOME,
+      }
+    ],
   },
-  {
-    path: '**',
-    redirectTo: 'auth',
-  }
 ];
