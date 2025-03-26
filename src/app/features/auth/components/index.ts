464c9a04735e85e251f1feb1faa2ef295325b2a5
KleberMera <klebermera2016@gmail.com>
2025-03-25 19:10:23 -0500

    feat: reorganizar importaciones de componentes en la página de recuperación de contraseña para mejorar la claridad y la estructura del código

--- src/app/features/auth/components/index.ts
+++ src/app/features/auth/components/index.ts
@@ -1,9 +1,5 @@
 //Todas las importaciones de los componentes de la pagina de login
 export * from './icons/loading-google/loading-google.component';
-export * from './password-reset/password-reset.component';
-export * from './success/success.component';
-export * from './user-validation/user-validation.component';
-export * from './verify-code/verify-code.component';
 export * from './icons/eye-slash/eye-slash.component';
 export * from './icons/eye/eye.component';
 export * from './icons/google/google.component';
--- src/app/features/auth/pages/forgot-password/forgot-password.component.ts
+++ src/app/features/auth/pages/forgot-password/forgot-password.component.ts
@@ -1,14 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 
-import {
-  UserValidationComponent,
-  VerifyCodeComponent,
-  PasswordResetComponent,
-  SuccessComponent,
-} from '../../components';
 import { ForgotPassService } from '../../services/forgot-pass.service';
 import { Router, RouterLink } from '@angular/router';
 import { AUTH_PAGES } from '../../auth.routes';
+import { PasswordResetComponent } from '../../components/password-reset/password-reset.component';
+import { SuccessComponent } from '../../components/success/success.component';
+import { UserValidationComponent } from '../../components/user-validation/user-validation.component';
+import { VerifyCodeComponent } from '../../components/verify-code/verify-code.component';
 
 @Component({
   selector: 'app-forgot-password',
@@ -28,4 +26,10 @@ export default class ForgotPasswordComponent {
   readonly pages = signal(AUTH_PAGES.LOGIN);
 
   stage = this._forgotService.stage;
+  
+  stageC = effect(() => {
+    if (this.pages() === AUTH_PAGES.LOGIN) {
+      this._forgotService.setStage('user-validation');
+    }
+  });
 }
