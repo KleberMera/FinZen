4eacbeadee5925c570ba45585a3ecda1ff852f57
KleberMera <klebermera2016@gmail.com>
2025-03-27 20:27:02 -0500

    feat: eliminar componentes y estilos no utilizados en la aplicación

--- src/app/shared/layout/navbar/navbar.component.ts
+++ src/app/shared/layout/navbar/navbar.component.ts
@@ -1,17 +1,19 @@
-import { Component, EventEmitter, inject, input, Output, output, signal } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { LogoComponent } from "../../icons/logo/logo.component";
-import { PushNotificationService } from '@services/push-notification.service';
-import { toast } from 'ngx-sonner';
-import { UserProfileSidebarComponent } from "./components/user-profile-sidebar/user-profile-sidebar.component";
-import { CommonModule } from '@angular/common';
-import { UserNotificationSidebarComponent } from "./components/user-notification-sidebar/user-notification-sidebar.component";
+import { LogoComponent } from '../../icons/logo/logo.component';
+import { UserProfileSidebarComponent } from './components/user-profile-sidebar/user-profile-sidebar.component';
+import { UserNotificationSidebarComponent } from './components/user-notification-sidebar/user-notification-sidebar.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, LogoComponent, UserProfileSidebarComponent, UserNotificationSidebarComponent],
+  imports: [
+    RouterLink,
+    LogoComponent,
+    UserProfileSidebarComponent,
+    UserNotificationSidebarComponent,
+  ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   sidebarOpen = input(false);
@@ -21,14 +23,12 @@ export class NavbarComponent {
   isUserNotificationOpen = signal(false);
 
   onUserClick() {
-    this.isUserSidebarOpen.update(value => !value);
+    this.isUserSidebarOpen.update((value) => !value);
   }
-
 
   onNotificationClick() {
-    this.isUserNotificationOpen.update(value => !value);
+    this.isUserNotificationOpen.update((value) => !value);
   }
-
 
   closeUserSidebar() {
     this.isUserSidebarOpen.set(false);
@@ -37,22 +37,4 @@ export class NavbarComponent {
   closeNotificationSidebar() {
     this.isUserNotificationOpen.set(false);
   }
-
-  private readonly pushService = inject(PushNotificationService);
-
-
-  // async onNotificationClick() {
-  //   const success = await this.pushService.requestSubscription();
-  //   if (success) {
-  //     console.log(success);
-      
-  //     console.log('Notificaciones activadas correctamente');
-  //     toast.success('Notificaciones activadas correctamente');
-  //   } else {
-  //     console.error('No se pudieron activar las notificaciones');
-  //     toast.error('No se pudieron activar las notificaciones');
-  //   }
-  // }
-  
-
 }
