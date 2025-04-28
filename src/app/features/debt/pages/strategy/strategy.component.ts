eed887494792f74332512094762c1991460aa1a5
KleberMera <klebermera2016@gmail.com>
2025-04-28 18:40:15 -0500

    feat: implementar página de estrategia de pago con métodos y botón de inicio

--- src/app/features/debt/pages/strategy/strategy.component.ts
+++ src/app/features/debt/pages/strategy/strategy.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export default class StrategyComponent {
 
+
+  startStrategy() {
+    // Aquí podrías navegar a un formulario de selección de deudas
+    console.log('Iniciar estrategia');
+    // ejemplo: this.router.navigate(['/seleccionar-deudas']);
+  }
+
 }
