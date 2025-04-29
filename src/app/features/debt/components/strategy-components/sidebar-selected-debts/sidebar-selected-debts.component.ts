fc803a81fff7b34f8f8cbfbd76cd386994487077
KleberMera <klebermera2016@gmail.com>
2025-04-29 17:43:11 -0500

    refactor: simplificar la lógica del componente de sidebar seleccionado de deudas

--- src/app/features/debt/components/strategy-components/sidebar-selected-debts/sidebar-selected-debts.component.ts
+++ src/app/features/debt/components/strategy-components/sidebar-selected-debts/sidebar-selected-debts.component.ts
@@ -1,8 +1,6 @@
 import { Component, computed, inject, output, signal } from '@angular/core';
 import { StorageService } from '@services/storage.service';
-import { NotificationService } from '../../../../../shared/layout/navbar/services/notification.service';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { DatePipe } from '@angular/common';
 import { format } from '@formkit/tempo';
 import { SnowballService } from '../../../services/snowball.service';
 import { apiResponse } from '@models/apiResponse';
@@ -18,40 +16,23 @@ export class SidebarSelectedDebtsComponent {
   closeSeletedDebtsSidebar = output<void>();
   protected readonly _storage = inject(StorageService);
   protected readonly _snowballService = inject(SnowballService);
-  lenguaje = signal<string>('es');
-  timeNow = signal<any>(new Date());
-
   seletdUserId = signal(this._storage.getUserId());
-  currentMonth = computed(() =>
-    format(this.timeNow(), 'MMMM', this.lenguaje())
-  );
-  close() {
-    console.log('closeSeletedDebtsSidebar');
+  currentMonth = computed(() => format(new Date(), 'MMMM', 'es'));
 
+  salary = rxResource<apiResponse<Salary>, { userId: number; currentMonth: string }>({
+    request: () => ({ userId: this.seletdUserId(), currentMonth: this.capitalizeFirstLetter(this.currentMonth())}),
+    loader: ({ request }) => this._snowballService.getSalaryByMonth(request.userId, request.currentMonth)});
+
+  debts = rxResource({
+    request: () => ({ userId: this.seletdUserId() }),
+    loader: ({ request }) => this._snowballService.getDebtByIdData(request.userId)});
+
+  close() {
     this.closeSeletedDebtsSidebar.emit();
   }
 
   capitalizeFirstLetter(value: string): string {
     return value.charAt(0).toUpperCase() + value.slice(1);
   }
-  salary = rxResource<
-    apiResponse<Salary>,
-    { userId: number; currentMonth: string }
-  >({
-    request: () => ({
-      userId: this.seletdUserId(),
-      currentMonth: this.capitalizeFirstLetter(this.currentMonth()),
-    }),
-    loader: ({ request }) =>
-      this._snowballService.getSalaryByMonth(
-        request.userId,
-        request.currentMonth
-      ),
-  });
-
-  debts = rxResource({
-    request: () => ({ userId: this.seletdUserId() }),
-    loader: ({ request }) =>
-      this._snowballService.getDebtByIdData(request.userId),
-  });
+  
 }
