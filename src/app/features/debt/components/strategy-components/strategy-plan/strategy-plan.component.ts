27ba047154c1302011f8f9fde6a1eea118e71140
KleberMera <klebermera2016@gmail.com>
2025-05-01 15:29:43 -0500

    feat: inyectar SnowballService y agregar lógica para cargar deudas con amortizaciones en StrategyPlanComponent

--- src/app/features/debt/components/strategy-components/strategy-plan/strategy-plan.component.ts
+++ src/app/features/debt/components/strategy-components/strategy-plan/strategy-plan.component.ts
@@ -1,5 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { StrategyMethod } from '@models/debt';
+import { SnowballService } from '../../../services/snowball.service';
 
 @Component({
   selector: 'app-strategy-plan',
@@ -9,6 +11,18 @@ import { StrategyMethod } from '@models/debt';
 })
 export class StrategyPlanComponent {
   dataProcess = input.required<StrategyMethod>();
+  protected readonly _snowballService = inject(SnowballService);
 
+  data = rxResource({
+    request: () => ({
+      userId: this.dataProcess().userId,
+      debtIds: this.dataProcess().debtIds,
+    }),
+    loader: ({request}) =>
+      this._snowballService.findDebtsByUserWithAmortizations(
+        request.userId,
+        request.debtIds
 
+      ),
+  });
 }
