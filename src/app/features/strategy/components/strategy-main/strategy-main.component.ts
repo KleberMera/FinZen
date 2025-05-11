7c9d1ef84d3d43413b2a18e3584d8a0a81e78fe3
KleberMera <klebermera2016@gmail.com>
2025-05-11 11:51:35 -0500

    feat: mejorar la carga de estrategias con un indicador de carga y optimizar la gestión de datos

--- src/app/features/strategy/components/strategy-main/strategy-main.component.ts
+++ src/app/features/strategy/components/strategy-main/strategy-main.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, effect, inject, signal } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { STRATEGY_PAGES } from '../../strategy.routes';
 import { StorageService } from '@services/storage.service';
 import StrategyStateService from '../../services/strategy-state.service';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-strategy-main',
@@ -10,26 +11,41 @@ import StrategyStateService from '../../services/strategy-state.service';
   templateUrl: './strategy-main.component.html',
   styleUrls: ['./strategy-main.component.scss'],
 })
-export default class StrategyMainComponent implements OnInit {
+export default class StrategyMainComponent {
   private router = inject(Router);
   private storage = inject(StorageService);
   private strategyState = inject(StrategyStateService);
   userId = signal<number>(this.storage.getUserId());
   activeTab = STRATEGY_PAGES.SNOWBALL;
 
-  ngOnInit() {
-    // Verificar si existe un plan guardado
-    this.strategyState.getPlan(this.userId()).subscribe({
-      next: (response) => {
-        if (response.data) {
-          // Si hay un plan, establecer los datos y navegar al plan
-          const planData = JSON.parse(response.data.datajson);
-          this.strategyState.setSelectedData(planData);
-          this.router.navigate(['home/plan']);
-        }
-      },
-      error: (error) => {
-        console.error('Error al obtener el plan:', error);
+  // ngOnInit() {
+  //   // Verificar si existe un plan guardado
+  //   this.strategyState.getPlan(this.userId()).subscribe({
+  //     next: (response) => {
+  //       if (response.data) {
+  //         // Si hay un plan, establecer los datos y navegar al plan
+  //         const planData = JSON.parse(response.data.datajson);
+  //         this.strategyState.setSelectedData(planData);
+  //         this.router.navigate(['home/plan']);
+  //       }
+  //     },
+  //     error: (error) => {
+  //       console.error('Error al obtener el plan:', error);
+  //     }
+  //   });
+  // }
+
+  dataStrategy = rxResource({
+    request: () => ({ userId: this.userId() }),
+    loader: ({ request }) => this.strategyState.getPlan(request.userId),
+  });
+
+  constructor() {
+    effect(() => {
+      if (this.dataStrategy.value()) {
+        const planData = JSON.parse(this.dataStrategy.value()?.data?.datajson!);
+        this.strategyState.setSelectedData(planData);
+        this.router.navigate(['home/plan']);
       }
     });
   }
