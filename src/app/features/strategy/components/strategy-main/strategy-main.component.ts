00c3141abe80976e367e215c0e0e7ff1e94a1f2c
KleberMera <klebermera2016@gmail.com>
2025-05-11 00:15:42 -0500

    feat: actualizar componentes de pestañas para usar enlaces y mejorar la navegación

--- src/app/features/strategy/components/strategy-main/strategy-main.component.ts
+++ src/app/features/strategy/components/strategy-main/strategy-main.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router, RouterModule, NavigationEnd } from '@angular/router';
 import { STRATEGY_PAGES } from '../../strategy.routes';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-strategy-main',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './strategy-main.component.html',
-  styleUrls: ['./strategy-main.component.scss'],  
+  styleUrls: ['./strategy-main.component.scss'],
 })
 export default class StrategyMainComponent {
   activeTab = STRATEGY_PAGES.SNOWBALL;
