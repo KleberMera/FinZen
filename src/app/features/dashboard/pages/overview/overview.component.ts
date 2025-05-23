435efe88d19ac8ca759c8e6b32fb45396a4a722b
KleberMera <klebermera2016@gmail.com>
2025-05-23 13:11:01 -0500

    feat(overview): simplificar componentes y lógica de rol en la vista de resumen

--- src/app/features/dashboard/pages/overview/overview.component.ts
+++ src/app/features/dashboard/pages/overview/overview.component.ts
@@ -1,20 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { DashboardHeaderComponent } from "../../components/dashboard-header/dashboard-header.component";
 import { CardSalaryComponent } from "../../components/card-salary/card-salary.component";
 import { CardMovementsComponent } from "../../components/card-movements/card-movements.component";
-import { CardDebtSummaryComponent } from "../../components/card-debt-summary/card-debt-summary.component";
-import { CardCategoriesDistributionComponent } from "../../components/card-categories-distribution/card-categories-distribution.component";
-import { TimelineTransactionComponent } from "../../components/timeline-transaction/timeline-transaction.component";
-import { CalendarComponent } from "../../components/calendar/calendar.component";
-import { CardNotificationComponent } from "../../components/card-notification/card-notification.component";
-import { CardGoalsComponent } from "../../components/card-goals/card-goals.component";
+import { StorageService } from '@services/storage.service';
 
 @Component({
   selector: 'app-overview',
-  imports: [DashboardHeaderComponent, CardSalaryComponent, CardMovementsComponent, CardDebtSummaryComponent, CardCategoriesDistributionComponent, TimelineTransactionComponent, CalendarComponent, CardNotificationComponent, CardGoalsComponent],
+  imports: [DashboardHeaderComponent, CardSalaryComponent, CardMovementsComponent],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss'
 })
 export default class OverviewComponent {
+  protected readonly _storageService = inject(StorageService);
+
+  rolIdUser = signal<number>(this._storageService.getRole()!);
 
 }
