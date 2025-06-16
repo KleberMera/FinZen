190e2fce636fddac2e4bf33bbdb676d173a344ec
KleberMera <klebermera2016@gmail.com>
2025-06-15 21:38:29 -0500

    refactor(chat): actualizar mensaje de bienvenida para incluir registro de múltiples transacciones

--- src/app/features/chat/services/chat.service.ts
+++ src/app/features/chat/services/chat.service.ts
@@ -9,7 +9,7 @@ import { Transaction } from '@models/transaction';
 export class ChatService {
   private botMessages = signal<Message[]>([{
     id: 1,
-    text: '¡Hola! 👋 Soy FinzenIA, tu asistente financiero personal. Por el momento puedo ayudarte a:\n\n• **Registrar transacciones** a través de texto o con una imagen 📝📸\n• **Responder preguntas generales** sobre finanzas personales 💡\n\n¿En qué puedo ayudarte hoy?',
+    text: '¡Hola! 👋 Soy FinzenIA, tu asistente financiero personal. Por el momento puedo ayudarte a:\n\n• **Registrar transacciones** a través de texto o con una imagen 📝📸\n• Registrar múltiples transacciones** a partir de una imagen o texto 📊🔄\n• **Responder preguntas generales** sobre finanzas personales 💡\n\n¿En qué puedo ayudarte hoy?',
     time: this.getCurrentTime(),
     type: 'bot',
     timestamp: Date.now()
