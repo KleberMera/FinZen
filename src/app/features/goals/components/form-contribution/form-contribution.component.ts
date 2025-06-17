4c9c251b1181faad5b8a908cdfa0315bd5dc7131
KleberMera <klebermera2016@gmail.com>
2025-06-17 16:57:30 -0500

    refactor(form-contribution): mejorar la legibilidad del código y ajustar la estructura de importaciones

--- src/app/features/goals/components/form-contribution/form-contribution.component.ts
+++ src/app/features/goals/components/form-contribution/form-contribution.component.ts
@@ -1,10 +1,18 @@
-import { Component, effect, EventEmitter, inject, input, Output, signal } from '@angular/core';
+import {
+  Component,
+  effect,
+  EventEmitter,
+  inject,
+  input,
+  Output,
+  signal,
+} from '@angular/core';
 import { GoalService } from '../../services/goal.service';
 import { GoalContribution } from '@models/meta';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { toast } from 'ngx-sonner';
-import { SideSheetContentComponent } from "../../../../shared/components/side-sheet/side-sheet-content.component";
+import { SideSheetContentComponent } from '../../../../shared/components/side-sheet/side-sheet-content.component';
 import { SolidColor } from '@models/styleClass';
 
 @Component({
@@ -17,56 +25,51 @@ export class FormContributionComponent {
   protected readonly _goalService = inject(GoalService);
   goalId = input.required<number>();
   userId = input.required<number>();
-  title = SolidColor
+  title = SolidColor;
   protected readonly form = this._goalService.formGoalContribution();
   protected isSaving = signal<boolean>(false);
-  
+
   @Output() onSave = new EventEmitter<void>();
   @Output() onCancel = new EventEmitter<void>();
-  
 
-    // Inicializar el formulario con el ID de la meta
+  // Inicializar el formulario con el ID de la meta
   constructor() {
     effect(() => {
       if (this.goalId()) {
-         this.form().patchValue({
-      goal_id: this.goalId()
-    });
+        this.form().patchValue({
+          goal_id: this.goalId(),
+        });
 
-         
-    console.log('Form initialized with goal_id:', this.goalId());
-    
-
+        console.log('Form initialized with goal_id:', this.goalId());
       }
     });
-
-    //  this.form().patchValue({
-    //   goal_id: this.goalId()
-    // });
   }
-  
+
   saveContribution() {
     if (this.form().invalid) return;
-    
+
     this.isSaving.set(true);
-    const contribution: GoalContribution = this.form().value as GoalContribution;
-    
-    this._goalService.createGoalContribution(contribution, this.userId()).subscribe({
-      next: (res) => {
-        this.isSaving.set(false);
-        toast.success(res.message);
-        this.form().controls['amount'].reset();
-        this.form().controls['date'].reset();
-        this.form().controls['note'].reset();
-        this.onSave.emit();
-      },
-    });
+    const contribution: GoalContribution = this.form()
+      .value as GoalContribution;
+
+    this._goalService
+      .createGoalContribution(contribution, this.userId())
+      .subscribe({
+        next: (res) => {
+          this.isSaving.set(false);
+          toast.success(res.message);
+          this.form().controls['amount'].reset();
+          this.form().controls['date'].reset();
+          this.form().controls['note'].reset();
+          this.onSave.emit();
+        },
+      });
   }
-  
+
   cancel() {
     this.form().controls['amount'].reset();
-        this.form().controls['date'].reset();
-        this.form().controls['note'].reset();
+    this.form().controls['date'].reset();
+    this.form().controls['note'].reset();
     this.onCancel.emit();
   }
 }
